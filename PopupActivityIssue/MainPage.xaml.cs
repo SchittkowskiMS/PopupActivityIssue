f6782d049db2a993ed251f793bfea2335fbf1ce1
Matthias Sachser <ms@office.schittkowski.de>
2024-08-20 14:57:46 +0200

    init

---
+++ PopupActivityIssue/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace PopupActivityIssue
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            var popup = new Popup()
+            {
+                Content = new Label() { Text = "Hello, World!" },
+            };
+
+            this.ShowPopup(popup);
+
+            await Task.Delay(10000);
+
+            popup.Close();
+        }
+    }
+
+}
